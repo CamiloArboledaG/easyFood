573116fb743ba82b142b9fabe3a4c2967911050a
camilo <camiloarboleda2000@gmail.com>
2021-05-01 17:52:21 -0500

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ImageBackground, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <ImageBackground source={require('./assets/primeraVista/fondo.png')} style={styles.image}>
+         <View style={styles.viewPrincipal}>
+
+         <View style={styles.viewText}>
+          <Text style={styles.TextMarca}>Easy Food</Text>
+         </View>
+        
+        <View style={styles.LogoView}>
+          <Image style={styles.Logo} source={require('./assets/primeraVista/Logo.png')} />
+        </View>
+
+          <View>
+          <TouchableOpacity style={styles.button}>
+             <Text style={styles.btntext}>Comencemos</Text>
+           </TouchableOpacity>
+          </View>
+         </View>
+       </ImageBackground>
     </View>
   );
 }
@@ -14,8 +30,48 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
   },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  viewPrincipal: {
+  },
+  LogoView: {
+    paddingTop:80,
+    paddingBottom:70,
+    height: 500,
+    alignItems:'center',
+  },
+  Logo: {
+    flex:1,
+    alignItems:'center',
+  },
+  TextMarca: {
+    fontSize: 60,
+    marginTop:50,
+    textShadowRadius: 5,
+    textShadowColor:"#363636"
+  },
+  
+  viewText: {
+    paddingBottom: 50,
+    alignItems:'center',
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: '#FFEBEE',
+    padding: 25,
+    borderRadius: 100,
+    marginTop:80,
+    margin:30,
+    },
+    btntext:{
+      color:"#FF5722",
+      fontSize:15,
+      fontWeight: 'bold'
+      
+    }
 });
